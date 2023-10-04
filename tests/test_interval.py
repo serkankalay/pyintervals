7a4d4c6888e3c77c0cb54ec1f5117d4986dc1027
Serkan Kalay <serkanosmankalay@gmail.com>
2023-10-04 17:25:39 +0200

    Restructuring tests.

--- tests/test_interval.py
+++ tests/test_interval.py
@@ -7,6 +7,22 @@ import pytest
 from pyintervals.interval import Interval
 
 
+@pytest.fixture
+def regular_interval() -> Interval:
+    return Interval(
+        start=datetime(2023, 1, 1, 9, 5),
+        end=datetime(2024, 1, 1, 9, 5),
+    )
+
+
+@pytest.fixture
+def degenerate_interval() -> Interval:
+    return Interval(
+        start=datetime(2023, 1, 1, 9, 5),
+        end=datetime(2023, 1, 1, 9, 5),
+    )
+
+
 def test_invalid_interval():
     with pytest.raises(RuntimeError):
         Interval(
@@ -15,19 +31,10 @@ def test_invalid_interval():
         )
 
 
-def test_valid_interval():
-    assert Interval(
-        start=datetime(2023, 1, 1, 9, 5),
-        end=datetime(2024, 1, 1, 9, 5),
-    )
+def test_valid_interval(regular_interval):
+    assert regular_interval
 
 
-def test_degenerate_interval():
-    assert Interval(
-        start=datetime(2023, 1, 1, 9, 5),
-        end=datetime(2023, 1, 1, 9, 5),
-    ).is_degenerate()
-    assert not Interval(
-        start=datetime(2023, 1, 1, 9, 5),
-        end=datetime(2023, 1, 1, 9, 6),
-    ).is_degenerate()
+def test_degenerate_interval(degenerate_interval, regular_interval):
+    assert degenerate_interval.is_degenerate()
+    assert not regular_interval.is_degenerate()
