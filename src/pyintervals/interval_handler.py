2598610c2e7bace0df0ed0dede4ecdafedf08485
Serkan Kalay <Kalay.Serkan@bcg.com>
2024-03-03 22:39:51 +0100

    WIP - started working on projection graph.

--- src/pyintervals/interval_handler.py
+++ src/pyintervals/interval_handler.py
@@ -1,17 +1,34 @@
 from __future__ import annotations
 
-from dataclasses import dataclass
+from dataclasses import dataclass, field
+from datetime import datetime
 from typing import Iterable, Collection
 
 from .interval import Interval
 
 
-@dataclass()
+@dataclass(frozen=True)
+class TimeValueIntervalNode:
+    time_point: datetime
+    __intervals: list[Interval] = field(default_factory=list)
+    
+    @property
+    def intervals(self) -> list[Interval]:
+        return list(self.__intervals)
+
+
+@dataclass
 class IntervalHandler:
     __intervals: list[Interval]
+    __projection_graph: list[TimeValueIntervalNode]
 
     def __init__(self, intervals: Iterable[Interval] = []):
-        self.__intervals = list(intervals)
+        self._initialize()
+        self.add(intervals)
+        
+    def _initialize(self) -> None:
+        self.__intervals = list()
+        self.__projection_graph = list()
 
     @property
     def intervals(self) -> list[Interval]:
