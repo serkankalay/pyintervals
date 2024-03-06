a7cb5748bc3ca6dbb3a03479450185d836d27200
Serkan Kalay <Kalay.Serkan@bcg.com>
2024-03-06 16:27:16 +0100

    Weak predecessor now covers more cases, possibly returning None.

--- src/pyintervals/search.py
+++ src/pyintervals/search.py
@@ -4,7 +4,11 @@ from typing import TypeVar, Sequence
 T = TypeVar("T")
 
 
-def weak_predecessor(sorted_sequence: Sequence[T], point: T) -> T:
+def weak_predecessor(sorted_sequence: Sequence[T], point: T) -> T | None:
+    # If the passed sequence is empty, then no predecessor.
+    if not sorted_sequence:
+        return None
+
     insertion_point = bisect.bisect_left(sorted_sequence, point)
     if insertion_point == len(sorted_sequence):
         # Then, we need to insert to the end of the collection.
@@ -14,7 +18,12 @@ def weak_predecessor(sorted_sequence: Sequence[T], point: T) -> T:
     if (current := sorted_sequence[insertion_point]) and current > point:
         # Then, we found a point which is the successor of our reference.
         # Hence, return the predecessor.
-        return sorted_sequence[insertion_point - 1]
+
+        # If the insertion point is found to be 0, then there is no predecessor
+        if insertion_point == 0:
+            return None
+        else:
+            return sorted_sequence[insertion_point - 1]
 
     # Otherwise, we found the correct point
     return current
--- tests/test_search.py
+++ tests/test_search.py
@@ -6,6 +6,8 @@ from pyintervals.search import weak_predecessor
 @pytest.mark.parametrize(
     "sequence, point, expected",
     [
+        ([], 0, None),
+        ([1, 2, 3, 4], 0, None),
         ([1, 2, 3, 4], 1, 1),
         ([1, 2, 3, 4], 3, 3),
         ([1, 2, 3, 4], 4, 4),
