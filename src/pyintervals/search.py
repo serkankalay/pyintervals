d25da4b40aa52ba4bcd85f1447cc6198b2ddceec
Sam de Wringer <deWringer.Sam@bcg.com>
2025-12-04 10:32:33 +0100

    Fix typehinting on `weak_predecessor` function.

--- src/pyintervals/search.py
+++ src/pyintervals/search.py
@@ -1,23 +1,26 @@
 from __future__ import annotations
 
 from bisect import bisect_left
-from typing import Sequence, TypeVar, cast
+from typing import TYPE_CHECKING, Sequence, Union
 
-T = TypeVar("T")
+if TYPE_CHECKING:
+    from _typeshed import SupportsRichComparisonT
 
 
-def weak_predecessor(sorted_sequence: Sequence[T], point: T) -> T | None:
+def weak_predecessor(
+    sorted_sequence: Sequence[SupportsRichComparisonT], point: SupportsRichComparisonT
+) -> Union[SupportsRichComparisonT, None]:
     # If the passed sequence is empty, then no predecessor.
     if not sorted_sequence:
         return None
 
-    insertion_point = bisect_left(sorted_sequence, point)  # type: ignore
+    insertion_point = bisect_left(sorted_sequence, point)
     if insertion_point == len(sorted_sequence):
         # Then, we need to insert to the end of the collection.
         # Hence, the weak predecessor is the last element in the list.
-        return cast(T, sorted_sequence[insertion_point - 1])
+        return sorted_sequence[insertion_point - 1]
 
-    if (current := sorted_sequence[insertion_point]) and current > point:
+    if (current := sorted_sequence[insertion_point]) and current > point:  # type: ignore[operator]
         # Then, we found a point which is the successor of our reference.
         # Hence, return the predecessor.
 
@@ -25,7 +28,7 @@ def weak_predecessor(sorted_sequence: Sequence[T], point: T) -> T | None:
         if insertion_point == 0:
             return None
         else:
-            return cast(T, sorted_sequence[insertion_point - 1])
+            return sorted_sequence[insertion_point - 1]
 
     # Otherwise, we found the correct point
-    return cast(T, current)
+    return current
